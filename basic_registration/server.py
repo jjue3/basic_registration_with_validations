9ab77f55f4af2125357f130a7d68c119ff1b7c15
jjue3 <46731286+jjue3@users.noreply.github.com>
2019-04-06 00:31:29 -0700

    added success page

--- basic_registration/server.py
+++ basic_registration/server.py
@@ -2,18 +2,26 @@ from flask import Flask, render_template, request, redirect, session, flash
 from mysqlconnection import connectToMySQL
 app = Flask(__name__)
 app.secret_key = 'secret_key'
-import re
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+import re 
 email_rex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 name_rex = re.compile(r'^[a-zA-Z]+$')    
 password_rex = re.compile(r"^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$])[\w\d@#$]{6,12}$")
 
-@app.route('/')
+@app.route('/') 
 def index():
-    return render_template('index.html')
+    return render_template('index.html')    
 
 @app.route('/', methods=['POST'])
 def registration():
     is_valid = True
+    mysql = connectToMySQL('reg')       
+    query = 'SELECT * FROM users WHERE email=%(email)s;'
+    data = {
+        "email" : request.form['email']
+    }
+    existing_email = mysql.query_db(query, data)
     if len(request.form['first_name']) < 2 or not name_rex.match(request.form['first_name']):
         is_valid = False
         flash("Please enter your first name")
@@ -28,7 +36,12 @@ def registration():
         is_valid = False
         flash("Please enter an email")
         print('false')
-        return redirect('/')    
+        return redirect('/')        
+    if existing_email:
+        is_valid = False
+        flash("Email already in use")
+        print('false')
+        return redirect('/')
     if not password_rex.match(request.form['password']):
         is_valid = False
         flash("Please enter a password")
@@ -50,16 +63,58 @@ def registration():
         session['last_name'] = request.form['last_name']
         session['email'] = request.form['email']
         session['password'] = request.form['password']
+        password = bcrypt.generate_password_hash(request.form['password'])
+        print(password)
         data = {
                 "first_name": request.form["first_name"],
                 "last_name": request.form["last_name"],
                 "email": request.form["email"],
-                "password": request.form["password"]
+                "password" : password
                 }
         mysql = connectToMySQL('reg')         
-        query = "INSERT INTO reg.users (first_name, last_name, email, password,  created_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW());"
-        successful = mysql.query_db(query, data)
+        query = "INSERT INTO reg.users (first_name, last_name, email, password, created_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW());"
+        userlog = mysql.query_db(query, data)
+        return redirect('/success')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if len(request.form['password'])<1:
+        flash("Please enter password")
+        return redirect('/')
+    if len(request.form['email'])<1:
+        flash("Please enter email")
+        return redirect('/')
+    mysql = connectToMySQL('reg')       
+    query = 'SELECT * FROM users WHERE email=%(email)s;'
+    data = {
+        "email" : request.form['email']
+        }
+    query_result = mysql.query_db(query, data)
+    if not query_result:
+        flash('Email/Password is incorrect')
+        return redirect('/')
+    pw_hash = query_result[0]["password"]
+    password_check = bcrypt.check_password_hash(pw_hash,request.form['password'])
+    if query_result:
+        if password_check:
+            flash('Success')
+            return redirect('/success')
+    else:
+        flash("Email/Password is incorrect")
         return redirect('/')
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    print(session)
+    return redirect('/')         
+
+@app.route('/success')
+def successload():
+    if session == "":
+        return redirect('/')
+    else:    
+        return render_template('success.html')
+    
 if __name__ == "__main__":
     app.run(debug=True)
